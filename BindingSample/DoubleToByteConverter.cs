806c8ed93f4e53e31363a07726ebe0abd2013a84
solitas <solitasroh@gmail.com>
2018-02-10 12:00:12 +0900

    initialize

---
+++ BindingSample/DoubleToByteConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Solitas
+{
+    [ValueConversion(typeof(double), typeof(byte))]
+    public class DoubleToByteConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null) return (byte)(double)value;
+            return byte.MinValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null) return (double)(byte)value;
+            return double.MinValue;
+        }
+
+        #endregion
+    }
+}
