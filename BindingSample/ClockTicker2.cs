bbabbe53fa36a243acee534836b1273fb7e759d2
solitas <solitasroh@gmail.com>
2018-02-10 13:25:04 +0900

    INotifyPrpoertyChanged 를 구현하여 바인딩 기능 작성

---
+++ BindingSample/ClockTicker2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using Solitas.Annotations;
+
+namespace Solitas
+{
+    public class ClockTicker2 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public DateTime DateTime => DateTime.Now;
+
+        public ClockTicker2()
+        {
+            var timer = new DispatcherTimer();
+            timer.Tick += (o, e) =>
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DateTime"));
+            };
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+        }
+    }
+}
