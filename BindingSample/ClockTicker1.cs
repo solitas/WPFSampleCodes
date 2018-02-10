9e45df7bcbe757c563cd6765b8abae37325cc089
solitas <solitasroh@gmail.com>
2018-02-10 13:06:07 +0900

    DependencyObject 를 통한 데이터 바인딩 기능 작성

---
+++ BindingSample/ClockTicker1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Solitas
+{
+    public class ClockTicker1 : DependencyObject
+    {
+        public static DependencyProperty DateTimeProperty =
+            DependencyProperty.Register("DateTime", typeof(DateTime), typeof(ClockTicker1));
+
+        public DateTime DateTime
+        {
+            get => (DateTime) GetValue(DateTimeProperty);
+            set => SetValue(DateTimeProperty, value);
+        }
+
+        public ClockTicker1()
+        {
+            var timer= new DispatcherTimer();
+            timer.Tick += (o,e) => { DateTime = DateTime.Now; };
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+        }
+    }
+}
